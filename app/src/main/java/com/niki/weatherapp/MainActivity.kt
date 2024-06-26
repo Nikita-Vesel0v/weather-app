f2b4d226285db6b68812d10e18df993f66e2d07a
Nikita Veselov <nikiveselov2004@gmail.com>
2024-06-26 21:33:26 +0300

    add simple response

--- app/src/main/java/com/niki/weatherapp/MainActivity.kt
+++ app/src/main/java/com/niki/weatherapp/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.niki.weatherapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.niki.weatherapp.databinding.MainActivityBinding
+import org.json.JSONObject
 
+const val API_KEY = "c2cb54503a3140859a8145001242606"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +18,27 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bGet.setOnClickListener {
+            getResult("London")
+        }
     }
-}
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json" +
+                "?key=$API_KEY&q=$name&aqi=no"
+        val stringRequest = StringRequest(Request.Method.GET,
+            url,
+            {
+                response->
+                    val obj = JSONObject(response)
+                    val temp = obj.getJSONObject("current")
+                    Log.d("AppLog", "Response: ${temp.getString("temp_c")}")
+            },
+            {
+                Log.d("AppLog", "Volley Error: $it")
+            }
+        )
+        val queue = Volley.newRequestQueue(this)
+        queue.add(stringRequest)
+
+    }
+ }
