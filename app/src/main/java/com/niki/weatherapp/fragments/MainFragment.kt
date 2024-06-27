a5e6bb170b373abcc7f5949caf704357fafe274b
Nikita Veselov <nikiveselov2004@gmail.com>
2024-06-27 08:08:03 +0300

    create adapter fot switching Days and Hours

--- app/src/main/java/com/niki/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/niki/weatherapp/fragments/MainFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.niki.weatherapp.adapters.VpAdapter
 import com.niki.weatherapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf("Hours", "Days")
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -25,8 +34,17 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
     }
 
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter= adapter
+
+        TabLayoutMediator(tabLayout, vp) {
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
+    }
     private fun permissionListener() {
         pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
             Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
